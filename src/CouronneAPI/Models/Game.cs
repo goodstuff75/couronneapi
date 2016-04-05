1ee8fc3c84ab3f1655e23f2c465b4b3c016a5e6a
goodstuff75 <gustaf.andersson@nansen.com>
2016-04-05 16:55:47 +0200

    validation

--- src/CouronneAPI/Models/Game.cs
+++ src/CouronneAPI/Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,11 @@ namespace CouronneAPI.Models
     {
 
         public int Id { get; set; }
+        [Required]
         public int Player1 { get; set; }
+        [Required]
         public int Player2 { get; set; }
+        [Required]
         public int Winner { get; set; }
         public DateTime PlayDate { get; set; }
     }
--- src/CouronneAPI/Models/Player.cs
+++ src/CouronneAPI/Models/Player.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CouronneAPI.Models
 {
     public class Player
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string UserName { get; set; }
         public DateTime Created { get; set; }
         public List<Game> Games { get; set; }
